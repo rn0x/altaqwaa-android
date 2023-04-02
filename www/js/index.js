ef3103c8fee8ab6fddb7ea7ff0a3bb31793d1a52
rn0x <ry2n1996@gmail.com>
2023-04-02 04:39:18 +0300

    fix

--- www/js/index.js
+++ www/js/index.js
@@ -50,34 +50,19 @@ document.addEventListener('deviceready', async (e) => {
 
     }
 
-    // await footer();
-    // await adhkar();
-    // await prayer();
-    // await quran();
-    // await more();
-    // await albitaqat();
-    // await hisnmuslim();
-    // await radio();
-    // await tfs();
-    // await images();
-    // await allah();
-    // await settings();
-    // await sabha();
-    // await notification();
+    await footer();
+    await adhkar();
+    await prayer();
+    await quran();
+    await more();
+    await albitaqat();
+    await hisnmuslim();
+    await radio();
+    await tfs();
+    await images();
+    await allah();
+    await settings();
+    await sabha();
+    await notification();
 
 }, false);
-
-await footer();
-await adhkar();
-await prayer();
-await quran();
-await more();
-await albitaqat();
-await hisnmuslim();
-await radio();
-await tfs();
-await images();
-await allah();
-await settings();
-await sabha();
-await notification();
