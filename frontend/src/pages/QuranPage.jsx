092414d68792053244ddb27758e38c7856e5b29a
rn0x <rn0x.me@gmail.com>
2024-09-29 07:57:07 +0300

    🚀 2024-09-29 07:57

--- frontend/src/pages/QuranPage.jsx
+++ frontend/src/pages/QuranPage.jsx
@@ -4,18 +4,23 @@ import useTheme from '../hooks/useTheme';
 import styles from '../styles/QuranPage.module.css';
 import metadataQuran from '../assets/json/metadataQuran.json';
 import pagesQuran from '../assets/json/pagesQuran.json';
+import useTranslation from '../hooks/useTranslation';
 
 export default function QuranPage() {
     const pages = pagesQuran;
     const { theme } = useTheme();
+    const { direction } = useTranslation();
     const [currentIndex, setCurrentIndex] = useState(0);
     const [startX, setStartX] = useState(0);
     const [endX, setEndX] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
-    const [imageScale, setImageScale] = useState(1); // حالة لتكبير الصورة
-    const [showNavigation, setShowNavigation] = useState(false); // حالة لإظهار أزرار التنقل
-    let hideTimeout; // مؤقت لإخفاء الأزرار
-    const [isSwiping, setIsSwiping] = useState(false); // حالة لتحديد إذا كان يتم سحب الصورة
+    const [imageScale, setImageScale] = useState(1);
+    const [showNavigation, setShowNavigation] = useState(false);
+    let hideTimeout;
+    const [isSwiping, setIsSwiping] = useState(false);
+
+    // حالة جديدة لتتبع ظهور الأزرار
+    const [areButtonsVisible, setAreButtonsVisible] = useState(false);
 
     useEffect(() => {
         const savedPage = localStorage.getItem('lastReadPage');
@@ -24,11 +29,31 @@ export default function QuranPage() {
             setCurrentIndex(Number(savedPage));
         }
 
-        return () => {
-            // تنظيف المؤقت عند تفريغ المكون
-            clearTimeout(hideTimeout);
+        // إضافة حدث keydown
+        const handleKeyDown = (event) => {
+            // إذا كان الاتجاه rtl
+            if (direction === 'rtl') {
+                if (event.key === 'ArrowRight') {
+                    handlePrev();
+                } else if (event.key === 'ArrowLeft') {
+                    handleNext();
+                }
+            } else { // إذا كان الاتجاه ltr
+                if (event.key === 'ArrowLeft') {
+                    handlePrev();
+                } else if (event.key === 'ArrowRight') {
+                    handleNext();
+                }
+            }
         };
-    }, []);
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            clearTimeout(hideTimeout);
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [currentIndex, hideTimeout, direction]);
 
     const handlePrev = () => {
         if (currentIndex > 0) {
@@ -51,20 +76,20 @@ export default function QuranPage() {
     const handleTouchStart = (e) => {
         const touch = e.touches[0];
         setStartX(touch.clientX);
-        setIsSwiping(false); // إعادة تعيين حالة السحب عند بدء اللمس
+        setIsSwiping(false);
     };
 
     const handleTouchMove = (e) => {
         const touch = e.touches[0];
         setEndX(touch.clientX);
-        setIsSwiping(true); // تعيين حالة السحب عند الحركة
+        setIsSwiping(true);
     };
 
     const handleTouchEnd = () => {
         const threshold = 50;
         const distance = startX - endX;
 
-        if (isSwiping) { // تحقق مما إذا كان هناك سحب
+        if (isSwiping) {
             if (distance < -threshold) {
                 handleNext();
             } else if (distance > threshold) {
@@ -105,7 +130,7 @@ export default function QuranPage() {
                         src={page.image.url}
                         aria-label={`Quran-Page-${page.page}`}
                         alt={`القرآن الكريم صفحة ${page.page}`}
-                        style={{ display: isLoading ? 'none' : 'block', transform: `scale(${imageScale})` }} // تطبيق مقياس الصورة
+                        style={{ display: isLoading ? 'none' : 'block', transform: `scale(${imageScale})` }}
                         className={`${styles.quranImage} ${theme === 'dark' ? styles.invert : ''}`}
                         onLoad={() => setIsLoading(false)}
                         onError={() => {
@@ -127,18 +152,20 @@ export default function QuranPage() {
         setImageScale(prevScale => Math.max(prevScale - 0.1, 0.1));
     };
 
-    // دالة لإظهار الأزرار
     const showButtons = () => {
-        setShowNavigation(true);
-        clearTimeout(hideTimeout); // إلغاء المؤقت القديم
-        hideTimeout = setTimeout(() => {
-            setShowNavigation(false); // إخفاء الأزرار بعد 5 ثواني
-        }, 5000);
+        if (!areButtonsVisible) {
+            setShowNavigation(true);
+            setAreButtonsVisible(true); // ضبط حالة الأزرار على مرئية
+            clearTimeout(hideTimeout);
+            hideTimeout = setTimeout(() => {
+                hideButtons();
+            }, 5000);
+        }
     };
 
-    // دالة لإخفاء الأزرار
     const hideButtons = () => {
         setShowNavigation(false);
+        setAreButtonsVisible(false); // ضبط حالة الأزرار على مخفية
     };
 
     return (
@@ -147,15 +174,15 @@ export default function QuranPage() {
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
             onTouchEnd={handleTouchEnd}
-            onMouseEnter={showButtons} // إظهار الأزرار عند المرور بالفأرة
-            onMouseLeave={hideButtons} // إخفاء الأزرار عند مغادرة الفأرة
-            onMouseMove={showButtons} // إظهار الأزرار عند تحريك الماوس
-            onFocus={showButtons} // إظهار الأزرار عند التركيز
-            onBlur={hideButtons} // إخفاء الأزرار عند فقدان التركيز
-            tabIndex={0} // السماح للعنصر بأن يكون قابلًا للتركيز
+            onMouseEnter={showButtons}
+            onMouseLeave={hideButtons}
+            onMouseMove={showButtons}
+            onFocus={showButtons}
+            onBlur={hideButtons}
+            tabIndex={0}
         >
             {renderPage(pages[currentIndex])}
-            {showNavigation && ( // إظهار أزرار التنقل
+            {showNavigation && (
                 <div className={styles.navigation}>
                     <button onClick={handlePrev} className={styles.navButton} disabled={currentIndex === 0}>
                         <FaArrowRight />
